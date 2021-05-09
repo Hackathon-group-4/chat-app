13f07734b3514cd4c54ed4c04dec17a18c9deab3
Andy <andrew.mcginn@gmail.com>
2021-05-09 02:11:26 +0100

    deployment config added to server app.js

--- app.js
+++ app.js
@@ -29,6 +29,17 @@ app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
 const index = require("./routes/index");
 app.use("/", index);
 
+
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
